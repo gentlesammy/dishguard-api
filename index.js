68d5d67e4c59fc5d655724f3e330d7c42ca349f4
Gentlesammy <samodunlade84@gmail.com>
2020-09-29 06:57:56 -1100

    basic authentication set

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
 //routes import
 const dishRoutes = require("./routes/dishRouter");
 const promoRoutes = require("./routes/promoRouter");
@@ -26,6 +27,32 @@ app.listen(9000, () => {
 });
 app.use(bodyParser.json());
 app.use(morgan("dev"));
+const auth = (req, res, next) => {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(401).json({
+      status: "error",
+      data: { message: "Unauthorized action" },
+    });
+  }
+  var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+  let username = auth[0];
+  let password = auth[1];
+  if (username == "admin" && password == "password") {
+    next();
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(401).json({
+      status: "error",
+      data: { message: "Invalid credentials" },
+    });
+  }
+};
+app.use(auth);
 app.use(express.static(__dirname + "/public"));
 /*
   #####ROUTES ##############
